f922c589b199e51fd075e9aef9208760c0d71607
Alfred K. <107874027+AlfredKimbork@users.noreply.github.com>
2026-03-13 08:41:29 +0100

    fetching data

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+
+const data =  await fetch(`https://api.themoviedb.org/3/trending/movie/day?language=en-us`, {
+    headers: {
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNGU5YjZmYjBjYzYzYTc3NTU3ZjQ0ZDk0NGJjY2ZjNCIsIm5iZiI6MTc3MzIyNDExMS4xNzkwMDAxLCJzdWIiOiI2OWIxNDBhZjQ1MjIyZmM1NzgwOTQyODIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.uTndF08_gAU7PP7HJ2QLDAXvM81W_iwGw-HVGDQocdQ',
+        accept: 'application/json'
+    }
+}).then(response => response.json());
+const res = data.results
+
+console.log(res);
+
